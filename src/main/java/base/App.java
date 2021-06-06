dbc28cc75316192e62fd4cfc5f2a4bca2036f5f0
carcmross <maarccross@gmail.com>
2021-06-06 19:29:09 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,60 @@
+package base;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Marc-Anthony Cross
+ */
+
+/* Quotation marks are often used to denote the start and end of a string. But sometimes we need to print out the quotation marks themselves by using escape characters.
+ *
+ *   Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
+ *   Example Output
+ *
+ *   What is the quote? These aren't the droids you're looking for.
+ *   Who said it? Obi-Wan Kenobi
+ *   Obi-Wan Kenobi says, "These aren't the droids you're looking for."
+ *
+ *   Constraints
+ *
+ *   Use a single output statement to produce this output, using appropriate string-escaping techniques for quotes.
+ *   If your language supports string interpolation or string substitution, don’t use it for this exercise. Use string concatenation instead.
+ *
+ *   Challenge
+ *
+ *   Modify this program so that instead of prompting for quotes from the user, you create a structure that holds quotes and their associated attributions and then display all of the quotes using the format in the example. An array of maps would be a good choice.
+ */
+
+public class App {
+    Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String quote = myApp.getQuote();
+        String author = myApp.getAuthor();
+        String outputString = myApp.generateOutput(author, quote);
+        myApp.printQuote(outputString);
+    }
+
+    private String generateOutput(String author, String quote) {
+        return author + " says, " + "\"" + quote + "\"";
+    }
+
+    private void printQuote(String outputString) {
+        System.out.println(outputString);
+    }
+
+    private String getQuote() {
+        System.out.print("What is the quote? ");
+        String quote = in.nextLine();
+        return quote;
+    }
+
+    private String getAuthor() {
+        System.out.print("Who said it? ");
+        String author = in.nextLine();
+        return author;
+    }
+}
